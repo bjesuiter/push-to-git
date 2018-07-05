18b8d34a6a947790c7974a7778aef7b511c991a8
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-07-06 00:42:48 +0200

    reformat to use tabs instead of simple spaces

--- src/cli.js
+++ src/cli.js
@@ -9,30 +9,30 @@ const projectVersion = require('project-version');
 // a command a to use it in command b from package.json script
 
 program
-    .version(projectVersion, '-v, --version')
-    .option('-t --target <target>', 'Git Target to push to, this can be a full git address or a registered git remote name')
-    .option('-b --branch <branch>', 'Target branch name to push to, defaults to the same name like input branch')
-    .option('-m --master', 'Sets the target branch to master per default')
-    .option('-d --dry-run', 'Emulates the upload - useful for checking input params')
-    .option('-p --production', 'Sets the production flag which triggers additional checks when uploading')
-    .option('-f --force', 'Forces git push');
+	.version(projectVersion, '-v, --version')
+	.option('-t --target <target>', 'Git Target to push to, this can be a full git address or a registered git remote name')
+	.option('-b --branch <branch>', 'Target branch name to push to, defaults to the same name like input branch')
+	.option('-m --master', 'Sets the target branch to master per default')
+	.option('-d --dry-run', 'Emulates the upload - useful for checking input params')
+	.option('-p --production', 'Sets the production flag which triggers additional checks when uploading')
+	.option('-f --force', 'Forces git push');
 
 program.on('--help', () => {
-    console.log(
-        `\n  General Information: 
+	console.log(
+		`\n  General Information: 
         Version: ${projectVersion}
         Purpose: Pushes the current branch to an arbitrary branch 
                  in the same or in another repository for deployment.
        `
-    )
+	)
 });
 
 program.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
-    //display the help text in red on the console
-    program.outputHelp();
-    process.exit(0);
+	//display the help text in red on the console
+	program.outputHelp();
+	process.exit(0);
 }
 
 // git address like or git remote name
@@ -45,69 +45,69 @@ const dryRun = program.dryRun;
 const isProduction = program.production;
 
 if (gitTarget === undefined) {
-    console.error('Git target address or remote name is missing');
-    process.exit(1);
+	console.error('Git target address or remote name is missing');
+	process.exit(1);
 }
 
 //git command to get the current branch name: git rev-parse --abbrev-ref HEAD
 spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD'], {capture: ['stdout', 'stderr']})
-    .then((result) => {
-        const currGitBranch = result.stdout.toString().trim();
+	.then((result) => {
+		const currGitBranch = result.stdout.toString().trim();
 
-        if (!currGitBranch) {
-            console.error('Current git branch is undefined!');
-            process.exit(2);
-        }
+		if (!currGitBranch) {
+			console.error('Current git branch is undefined!');
+			process.exit(2);
+		}
 
-        if (gitTargetBranch === undefined) {
-            gitTargetBranch = (targetBranchDefault) ? 'master' : currGitBranch;
-        }
+		if (gitTargetBranch === undefined) {
+			gitTargetBranch = (targetBranchDefault) ? 'master' : currGitBranch;
+		}
 
-        console.log(`Push current git branch [${currGitBranch}] to ${gitTargetBranch} of: \n` +
-            `${gitTarget}`);
+		console.log(`Push current git branch [${currGitBranch}] to ${gitTargetBranch} of: \n` +
+			`${gitTarget}`);
 
-        const gitParams = [
-            'push'
-        ];
+		const gitParams = [
+			'push'
+		];
 
-        if (force) {
-            gitParams.push('-f');
-        }
+		if (force) {
+			gitParams.push('-f');
+		}
 
-        gitParams.push(`${gitTarget}`);
-        gitParams.push(`${currGitBranch}:${gitTargetBranch}`);
+		gitParams.push(`${gitTarget}`);
+		gitParams.push(`${currGitBranch}:${gitTargetBranch}`);
 
-        console.log(`The exact command is: 
+		console.log(`The exact command is: 
         git ${gitParams.join(' ')}`);
 
-        if (isProduction) {
-            return prompt(`CAUTION: You are updating a production branch! Proceed? (yes | NO) `)
-                .then((value) => {
-                    if (value === 'yes' || value === 'y') {
-                        return runGitPush(gitParams)
-                    } else {
-                        console.log('Update canceled by user');
-                    }
-                });
-        } else {
-            return runGitPush(gitParams);
-        }
-    }).catch(function (err) {
-    const error = (err.stderr) ? err.stderr : err;
-    console.error('Execution Errors: ', error);
+		if (isProduction) {
+			return prompt(`CAUTION: You are updating a production branch! Proceed? (yes | NO) `)
+				.then((value) => {
+					if (value === 'yes' || value === 'y')
+						return runGitPush(gitParams);
+					
+					console.log('Update canceled by user');
+
+				});
+		} else {
+			return runGitPush(gitParams);
+		}
+	}).catch(function (err) {
+	const error = (err.stderr) ? err.stderr : err;
+	console.error('Execution Errors: ', error);
 });
 
 function runGitPush(gitParams) {
-    if (dryRun) {
-        console.log('Dry Run finished');
-        process.exit(0);
-    }
+	if (dryRun) {
+		console.log('Dry Run finished');
+		process.exit(0);
+	}
 
-    const gitPromise = spawn('git', gitParams);
-    const childProcess = gitPromise.childProcess;
+	const gitPromise = spawn('git', gitParams);
+	const {childProcess} = gitPromise;
 
-    childProcess.stdout.pipe(process.stdout);
-    childProcess.stderr.pipe(process.stderr);
+	childProcess.stdout.pipe(process.stdout);
+	childProcess.stderr.pipe(process.stderr);
 
-    return gitPromise;
+	return gitPromise;
 }
