0fb35086e7cba50924c9a6abd2991c76d67cbf00
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-13 18:22:18 +0200

    make getCurrentBranchName async and add assebleGitPush function

--- src/git-adapter.js
+++ src/git-adapter.js
@@ -1,7 +1,42 @@
 import {spawn} from 'child-process-promise';
 
-export function getCurrentBranchName() {
-	return spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD'], {
+/**
+ * Git command to get the current branch name: git rev-parse --abbrev-ref HEAD
+ */
+export async function getCurrentBranchName() {
+	const result = await spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD'], {
 		capture: ['stdout', 'stderr'],
 	});
+
+	return result.stdout.toString().trim();
+}
+
+/**
+ * @param extraGitOptions string which gets appended to the git push command
+ */
+export function assembleGitPush(targetUrl, sourceBranch, targetBranch, force, extraGitOptions) {
+	const gitParameters = ['push'];
+
+	if (force) {
+		gitParameters.push('-f');
+	}
+
+	if (extraGitOptions !== undefined) {
+		gitParameters.push(extraGitOptions);
+	}
+
+	gitParameters.push(`${targetUrl}`);
+	gitParameters.push(`${sourceBranch}:${targetBranch}`);
+
+	return gitParameters;
+}
+
+export function gitPush(gitParameters) {
+	const gitPromise = spawn('git', gitParameters);
+	const {childProcess} = gitPromise;
+
+	childProcess.stdout.pipe(process.stdout);
+	childProcess.stderr.pipe(process.stderr);
+
+	return gitPromise;
 }
