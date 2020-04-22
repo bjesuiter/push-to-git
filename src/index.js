71875c459e25e012e7e66ce2e1c2c28b332a3561
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-22 14:28:23 +0200

    restructure index.js location

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 // Set options as a parameter, environment variable, or rc file.
 
-// This is a hack from the esm package enables es module loading for all other imports in src/cli.js
+// This is a hack from the esm package enables es module loading for all other imports in cli.js
 // eslint-disable-next-line no-global-assign
 require = require('esm')(module);
-module.exports = require('./src/cli.js');
+module.exports = require('./cli.js');
