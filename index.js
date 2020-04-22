5e5a38b135c42e206cd0d6cfbf7701085e8257d1
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-22 14:25:36 +0200

    allow esm hack in index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
 // Set options as a parameter, environment variable, or rc file.
-require = require('esm')(module /*, options*/);
+
+// This is a hack from the esm package enables es module loading for all other imports in src/cli.js
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module);
 module.exports = require('./src/cli.js');
